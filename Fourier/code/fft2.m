081ca71077c5ee8aa894e6ac3429fb3def9a01ea
Alesh <xryska01@stud.feec.vutbr.cz>
2021-04-08 10:39:55 +0200

    Fourier

--- Fourier/code/fft2.m
+++ Fourier/code/fft2.m
@@ -1,50 +1,48 @@
-%close all;
+close all;
 clear all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %CONSTS
-f_sample = 100; %hz
-f_signal = 1/10;
-T = 1/f_signal;
-
-t = 0:1/f_sample:(4*pi);
-
-Amp = 20;
-DC = 20;
-
-w0 = 1;
+A=1;
+f = 0;
+f_signal = 100;
 
 N3 = 3;
 N10 = 10;
 N30 = 30;
+
+Ns=5;
+t=linspace(-5,5,1000);
+T=max(t);
+w0 = (3*pi)/T;
+pix2 = 2*pi;
+
+Amp = 2;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %SIGNAL
-y = DC+Amp*sawtooth((1/(2*f_signal*pi))*t);
+y = Amp*sawtooth((1/(2*f_signal*pi))*t);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-myFunc=@(t) (4*t);
-%  myFunc=@(t) exp(-t);
-% myFunc=@(t) exp(-t).*sin(10*t);
-y=myFunc(t);
+for k=1:Ns
+  ak = ((4*A*sin(pix2*k) / (pix2*k)) + (4*A*cos(pix2*k) / (pix2*k)^2)-(4*A/(pix2*k)^2));
+  bk = ((4*A*sin(pix2*k) / (pix2*k)^2) + (cos(pix2*k) / (pix2*k)));
+  f = f+((ak*cos(k*w0*t)) - bk*sin(k*w0*t));
+  
+  fprintf('\n------------------------------------------------------\n');
+  fprintf('Coeff a %d: ',k)
+  fprintf('%G',ak)
+  fprintf('\n');
+  
+  fprintf('Coeff b %d: ',k)
+  printf('%G',bk)
+end
 
-A0=(2/T)*quadv(myFunc,0,4*pi);
+for k=1:Ns
+  ak = 2./T*y*cos(k*w0*t);
+end
 
-%subplot(3,1,1);
-%plot(t,y);
-%hold on;
-%plot(t,fouri(N3,y_f,a,t,w0));
-%title(strcat('Fourierova řada pro N = ',' ',num2str(N3)));
-%grid on;
-%
-%subplot(3,1,2);
-%plot(t,y);
-%hold on;
-%plot(t,fouri(N10,y_f,a,t,w0));
-%title(strcat('Fourierova řada pro N = ',' ',num2str(N10)));
-%grid on;
-%
-%subplot(3,1,3);
-%plot(t,y);
-%hold on;
-%plot(t,fouri(N30,y_f,a,t,w0));
-%title(strcat('Fourierova řada pro N = ',' ',num2str(N30)));
-%grid on;
+
+figure(1)
+plot(t,y)
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
